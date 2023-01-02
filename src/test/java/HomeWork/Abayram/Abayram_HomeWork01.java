36f430096c6c15718a871df444d07bf025f88a58
Adem Yusuf <adiguzelademyusuf@gmail.com>
2023-01-02 21:40:02 +0300

    01/02/2023

--- src/test/java/HomeWork/Abayram/Abayram_HomeWork01.java
+++ src/test/java/HomeWork/Abayram/Abayram_HomeWork01.java
@@ -1,7 +1,10 @@
 package HomeWork.Abayram;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.AcceptedW3CCapabilityKeys;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import utilities.TestBase;
 
@@ -18,4 +21,25 @@ public class Abayram_HomeWork01 extends TestBase {
         Sonrasinda paragrafa “iframein icindeyim” yazisini yazdıralım
         Alt kısımdaki yazının ‘Elemental Selenium’ yazisini içerdiğini test edelim
      */
+
+    @Test
+    public void test1() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/iframe");
+        WebElement iframeCount = driver.findElement(By.xpath("//iframe"));
+        System.out.println(iframeCount);
+
+        String pageText = driver.findElement(By.xpath("//h3[.='An iFrame containing the TinyMCE WYSIWYG Editor']")).getText();
+        Assert.assertTrue(pageText.contains("Editor"));
+
+        WebElement iframe = driver.findElement(By.id("mce_0_ifr"));
+        driver.switchTo().frame(iframe).findElement(By.xpath("//body[@data-id='mce_0']")).sendKeys(Keys.END,Keys.LEFT_SHIFT,Keys.HOME,Keys.DELETE,Keys.LEFT_SHIFT,"iframein icindeyim");
+
+        driver.switchTo().parentFrame();
+
+        String pageDowntext = driver.findElement(By.xpath("//div[@style='text-align: center;']")).getText();
+        String expected = "Elemental Selenium";
+        Assert.assertTrue(pageDowntext.contains(expected));
+
+    }
+
 }
--- src/test/java/HomeWork/Abayram/Abayram_HomeWork02.java
+++ src/test/java/HomeWork/Abayram/Abayram_HomeWork02.java
@@ -1,6 +1,14 @@
 package HomeWork.Abayram;
 
-public class Abayram_HomeWork02 {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+import java.util.Set;
+
+public class Abayram_HomeWork02 extends TestBase {
 
     /*
         https://testcenter.techproeducation.com/index.php?page=multiple-windows
@@ -8,4 +16,33 @@ public class Abayram_HomeWork02 {
         Click here a tiklayin
         Sonra açılan sayfadaki title in ‘New Window’ oldugunu dogrulayin
      */
+
+    @Test
+    public void test1(){
+        driver.get("https://testcenter.techproeducation.com/index.php?page=multiple-windows");
+
+        String parentWindow = driver.getWindowHandle();
+
+        String pageTitle = driver.getTitle();
+        Assert.assertEquals("Windows", pageTitle);
+
+        driver.findElement(By.xpath("//a[@href='index.php?page=new-windows']")).click();
+
+        Set<String> windows = driver.getWindowHandles();
+
+        for (String w: windows){
+
+            if (!w.equals(parentWindow)){
+                driver.switchTo().window(w);
+                break;
+            }
+        }
+
+        String childTitle = driver.getTitle();
+        Assert.assertEquals("New Window", childTitle);
+
+
+    }
+
+
 }
